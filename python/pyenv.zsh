42b2040ff5d7724b0565aebcad940f2492302cfa
Ryan C. Creasey <ryan@kaneda.net>
2015-12-21 23:39:32 -0800

    Pruning more brew packages; adding pyenv and git-lfs.

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,6 +1,7 @@
 # init according to man page
 if (( $+commands[pyenv] ))
 then
-  export PATH="$HOME/.pyenv/bin:$PATH"
+  export PYENV_ROOT=/usr/local/var/pyenv
   eval "$(pyenv init -)"
+  if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 fi

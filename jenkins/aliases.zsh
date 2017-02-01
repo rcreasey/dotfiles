923f63d3306593715d1d769278b5d53d80682197
Ryan C. Creasey <rcreasey@blizzard.com>
2017-02-01 10:57:39 -0800

    updates.

--- jenkins/aliases.zsh
+++ jenkins/aliases.zsh
@@ -1,3 +1,2 @@
-alias auth-t1-jenkins="source ~/.dotfiles/secrets/t1-jenkins.sh"
 alias auth-s2-jenkins="source ~/.dotfiles/secrets/s2-jenkins.sh"
-alias auth-hero-jenkins="source ~/.dotfiles/secrets/hero-jenkins.sh"
+source ~/.dotfiles/secrets/hero-jenkins.sh

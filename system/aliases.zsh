4c1a82682bcb70df6ce6f6593bb5a81d38ccf4c6
Ryan C. Creasey <ryan@kaneda.net>
2019-02-02 18:47:36 -0800

    Polishing random-wallpaper a bit

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,9 @@ alias db="docker build -t \"${PWD##*/}\" ."
 alias docker-purge="docker rm -f $(docker ps -qa); docker rmi -f $(docker images -qa)"
 
 alias apu="apm update --no-confirm"
+alias neo="neofetch --backend iterm2"
 
-alias wpu="/Users/rcreasey/.dotfiles/system/random-wallpaper.sh"
+alias wpu="/Users/rcreasey/.dotfiles/system/random-wallpaper"
 test -e "${HOME}/.cache/wal/sequences" && (cat $HOME/.cache/wal/sequences &)
 
 

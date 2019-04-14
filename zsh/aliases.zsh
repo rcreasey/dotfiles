de0482d18f5b457abd8506b3290e1bf10a4bde1b
Ryan C. Creasey <ryan@kaneda.net>
2019-04-14 15:22:37 -0700

    Updating zsh, vim and random-wallpaper

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,12 +4,10 @@ alias -g ....='../../..'
 alias -g .....='../../../..'
 
 alias ls="exa"
-#alias ll="exa -l"
-#alias la="exa -a"
+alias ll="exa -l"
 alias lt="exa --tree"
 
 alias l="k"
-alias ll="k -l"
 alias la="k -a"
 
 alias atom="code"
